e751fa7dbcc6c1881321d96481b5ce18b8d15563
Aaron Freidus <aaronf101@gmail.com>
2017-06-25 21:51:00 -0400

    complete it's always sunny bot deployable on lambda

--- handler.js
+++ handler.js
@@ -1,53 +1,83 @@
 'use strict';
 
 const request = require('request'),
-      svg2png = require('svg2png'),
-      fs = require('fs');
+      gm = require('gm').subClass({ imageMagick: true });
 
-module.exports.sunny = (event, context) => {
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
-  let text = '',
-      callback = () => {
-        //TODO send a response to slack
-      }
+function parseQuery(qstr) {
+    var query = {};
+    var a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
+    for (var i = 0; i < a.length; i++) {
+        var b = a[i].split('=');
+        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    }
+    return query;
+}
 
-  function uploadImageToImgur(str = '1', callback)
-  {
-    let textSplit = str.match(/.{1,10}/g),
-    textMap = textSplit.map((text, index) => `<text font-size="35" font-family="Courier" x="200" y="${ 200 + (index * 50) }" fill="white" > ${ text } </text>`).join(''),
-    svgTemplate = `
-    <svg width="500" height="500">
-      <rect width="500" height="500" fill="black" />
-      ${ textMap }
-    </svg>
-    `
-      svg2png(new Buffer( svgTemplate ))
-     .then( buffer => buffer.toString('base64') )
-     .then((base64Image) => {
+function _transformStringToFit(str) {
+    let dividedString = str.split(' '),
+     lines = [""],
+     currentLine = 0;
+
+    dividedString.forEach( (word) => {
+        if (lines[currentLine].length + word.length <= 26) {
+        lines[currentLine] = lines[currentLine] + " " + word;
+    } else {
+        currentLine = currentLine + 1;
+        lines[currentLine] = word;
+    }
+    });
+
+    return lines.join('\n');
+}
+
+module.exports.sunny = (event, context, cb) => {
+  let body = parseQuery(event.body),
+  text = _transformStringToFit(body.text.replace(/\+/g, " ")),
+  responseUrl = body.response_url,      
+  postToSlack = (linkToImage) => {
         let options = {
+          url: responseUrl,
+          method: 'POST',
+          json: true,
+          body: {
+            "text": "New Shitpost Alert!",
+            "response_type": "in_channel",
+            "attachments": [
+              {
+                "title": "It's Always Sunny In Slack",
+                "image_url": `${linkToImage}`
+              }
+            ]
+          }
+        };
+        request(options, (e,d) => cb(null))
+  },
+  uploadImageToImgur = (str = 'fooo bar', callback = null) => {
+    gm( 500, 500, '#000' )
+      .fill('#fff')
+      .font('./Textile.ttf')
+      .fontSize(30)
+      .drawText(0, 0, text,'Center')
+      .border(2, 2).borderColor('#ff0000')
+      .toBuffer('png', (err, buffer) =>  {
+          let base64Encode = buffer.toString('base64'),
+          options = {
            url: 'https://api.imgur.com/3/image',
            method: 'POST',
            headers: {
               'User-Agent': 'request',
-              'authorization': `Client-ID ${ process.env.IMGUR_CLIENT_ID }`,
+              'authorization': `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
            },
-           json: true,
-           body: {
+          json: true,
+          body: {
             type: 'base64',
-            image: base64Image
+            image: base64Encode
           }
         }
-        request(options, (e, d) => console.log(d.body.data.link) )
-    })
-    .catch( e => console.log( e ))
+        request(options, (e, d) => {
+           callback(d.body.data.link)
+        })
+      });
   }
-  uploadImageToImgur(text, callback)
+  uploadImageToImgur(text, postToSlack)
 };
-
-
