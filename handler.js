9ca64c4d145e76aecc82c8524371707c52bf0c91
Aaron Freidus <aaronf101@gmail.com>
2017-07-10 11:09:07 -0400

    updated package.json to include deploy script

--- handler.js
+++ handler.js
@@ -8,10 +8,10 @@ const request = require('request'),
 Function to parse out query string parameters into object key pairs. 
 **/
 function parseQuery(qstr = '') {
-    var query = {};
-    var a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
+    let query = {},
+    a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
     for (var i = 0; i < a.length; i++) {
-        var b = a[i].split('=');
+        let b = a[i].split('=');
         query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
     }
     return query;
